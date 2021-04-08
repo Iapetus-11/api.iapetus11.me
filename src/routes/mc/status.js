6b984d87767ab5d93a1050d09e527877beddd24f
Milo Weinberg <iapetus011@gmail.com>
2021-04-08 19:48:06 -0400

    update mcstatus route

--- src/routes/mc/status.js
+++ src/routes/mc/status.js
@@ -1,12 +1,16 @@
 import express from "express";
 
+import {mcStatus, validateAddress} from "../../util/mc/common.js";
+
 const router = express.Router();
 
 router.get("/:address", (req, res) => {
   const address = req.params.address;
 
-  if (40 <= address.length <= 4) {
-    res.status(400).json({success: false, message: "Bad Request - URL parameter address is invalid."});
+  try {
+    validateAddress(address);
+  } catch (e) {
+    res.status(400).json({success: false, message: "Bad Requests - URL parameter address is invalid."});
     return;
   }
 
