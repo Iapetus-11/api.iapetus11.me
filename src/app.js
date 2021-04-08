3801b2406a3ab8b24b00e62e00fb3e2e94a3974e
Milo Weinberg <iapetus011@gmail.com>
2021-04-08 19:31:00 -0400

    use mcstatus route

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 import helmet from "helmet";
 
+// import routes
+import routeMCStatus from "./routes/mc/status.js";
+
 // load .env data
 dotenv.config();
 
@@ -9,6 +12,9 @@ const app = express();
 
 // use middleware
 app.use(helmet());
+
+// add routes
+app.use("/mc/status", routeMCStatus);
 
 app.use((req, res) => { // handle 404s, must be after middleware and routes to work
   res.status(404).json({success: false, message: 'Error - Endpoint not found or method not supported for this endpoint.'});
