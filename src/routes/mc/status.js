40f965048802c8c864de07b0d8b415e9509de4ea
Milo Weinberg <iapetus011@gmail.com>
2021-04-08 19:20:32 -0400

    start on server status route

---
+++ src/routes/mc/status.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/:address", (req, res) => {
+  const address = req.params.address;
+
+  if (text.length <= 4) {
+    res.status(400).json({success: false, message: "Bad Request - URL parameter address is invalid."});
+    return;
+  }
+
+  res.status(200).json({success: true, message: "bruh"});
+});
