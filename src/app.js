0981d12e83b3935106840d8e7e3706821389fd8b
Milo Weinberg <iapetus011@gmail.com>
2021-04-08 19:12:36 -0400

    add helmet

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
+import helmet from "helmet";
 
 // load .env data
 dotenv.config();
 
 const app = express();
+
+// use middleware
+app.use(helmet());
 
 // run app
 app.listen(process.env.PORT, () => {
