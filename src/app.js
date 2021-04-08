f0affd8820d4c67794ec9d1505a0618cb3cf3d3f
Milo Weinberg <iapetus011@gmail.com>
2021-04-08 19:25:03 -0400

    add 404 response

--- src/app.js
+++ src/app.js
@@ -10,6 +10,10 @@ const app = express();
 // use middleware
 app.use(helmet());
 
+app.use((req, res) => { // handle 404s, must be after middleware and routes to work
+  res.status(404).json({success: false, message: 'Error - Endpoint not found or method not supported for this endpoint.'});
+});
+
 // run app
 app.listen(process.env.PORT, () => {
   console.log(`Petu-API started on port ${process.env.PORT}`);
