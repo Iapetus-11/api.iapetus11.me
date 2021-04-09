5f829247932d99f413ab3b164676fd81086950c3
Milo Weinberg <iapetus011@gmail.com>
2021-04-08 20:15:20 -0400

    add parseAddress()

--- src/util/mc/common.js
+++ src/util/mc/common.js
@@ -1,18 +1,33 @@
+import {bedrockServerStatus} from "./bedrock/status.js";
 
 const validAddressChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890:.";
 
-export const validateAddress = (address) => {
+export const parseAddress = (address) => {
   if (4 >= address.length > 30) {
     throw new Error();
   }
 
+  let colonCount = 0;
+
   for (let c of address) {
+    if (c === ":") {
+      colonCount += 1;
+    }
+
     if (!validAddressChars.includes(c)) {
       throw new Error();
     }
   }
+
+  if (colonCount > 1) {
+    throw new Error();
+  } else if (colonCount === 1) {
+    return address.split(":");
+  } else {
+    return [address, null];
+  }
 }
 
-export const mcStatus = (address) => {
-  throw new Error("Not implemented yet...");
+export const mcStatus = async (address) => {
+  return await bedrockServerStatus()
 }
