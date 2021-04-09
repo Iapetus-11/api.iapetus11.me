dab713eddeb9ddbf7c48afb979e569b2a47dea9f
Milo Weinberg <iapetus011@gmail.com>
2021-04-09 10:24:14 -0400

    more work on Connection class

--- src/util/mc/java.js
+++ src/util/mc/java.js
@@ -4,9 +4,41 @@ class Connection {
   constructor(host, port) {
     this.host = host;
     this.port = port;
+
+    this.sock = net.createConnection({host: host, port: port});
+
+    this.buffer = new Buffer();
+    this.pos = 0;
+
+    this.sock.on("data", (data) => {
+      this.buffer = Buffer.concat([this.buffer, Buffer.from(data)]);
+    });
+  }
+
+  readRaw(n) {
+    if (this.buffer.length >= this.pos + n) {
+      this.pos += n;
+      return this.buffer.slice(this.pos - n, this.pos);
+    } else {
+      throw new Error();
+    }
+  }
+
+  read(n) {
+    process.nextTick(() => {
+      try {
+        return readRaw(n)
+      } catch(e) {
+        return read(n);
+      }
+    });
+  }
+
+  write(d) {
+    return this.sock.write(d);
   }
 }
 
 export const javaServerStatus = (host, port) => {
-  const socket = null;
+  const con = new Connection(host, port);
 };
