956fbb126f713a550e195aee0d0daa1d46a142b1
Milo Weinberg <iapetus011@gmail.com>
2021-04-10 12:04:08 -0400

    add methods for reading + writing varints

--- src/util/mc/java.js
+++ src/util/mc/java.js
@@ -25,7 +25,7 @@ class Connection {
   }
 
   read(n) {
-    process.nextTick(() => {
+    setImmediate(() => {
       try {
         return readRaw(n);
       } catch (e) {
@@ -35,10 +35,61 @@ class Connection {
   }
 
   write(d) {
-    return this.sock.write(d);
+    return new Promise((resolve, reject) => {
+      this.sock.write(d, () => {
+        resolve();
+      });
+    });
+  }
+
+  writeVarInt(n, maxBits) {
+    maxBits = (maxBits ? maxBits : 32);
+    const numMin = (-1 << (maxBits - 1));
+    const numMax = (1 << (maxBits - 1));
+
+    if (!(numMin <= num < numMax)) {
+      throw new RangeError(`${num} doesn't fit in the range ${numMin} <= ${num} < ${numMax}`);
+    }
+
+    let out = new Buffer();
+
+    for (let i = 0; i < 10; i++) {
+      let b = num & 0x7F;
+      num = num >> 7;
+
+      out.writeUInt8(b | (num > 0 ? 0x80 : 0));
+
+      if (num == 0) break;
+    }
+
+    return out;
+  }
+
+  readVarInt(n, maxBits) {
+    maxBits = (maxBits ? maxBits : 32);
+    const numMin = (-1 << (maxBits - 1));
+    const numMax = (1 << (maxBits - 1));
+
+    let num = 0;
+
+    for (let i = 0; i < 10; i++) {
+      let b = this.read(1).readUInt8();
+      num = num | (b & 0x7F) << 7 * i;
+
+      if (!(b & 0x80)) break;
+    }
+
+    if (num & (1 << 31)) num -= (1 << 32);
+
+    if (!(numMin <= num < numMax)) {
+      throw new RangeError(`${num} doesn't fit in the range ${numMin} <= ${num} < ${numMax}`);
+    }
+
+    return num;
   }
 }
 
 export const javaServerStatus = (host, port) => {
   const con = new Connection(host, port);
+  let buffer = new Buffer();
 };
