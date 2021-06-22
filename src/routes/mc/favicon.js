79009950cd9bbdfe0eca88f803702f611068d12e
Milo Weinberg <iapetus011@gmail.com>
2021-06-22 12:35:34 -0400

    don't use await in non async function

--- src/routes/mc/favicon.js
+++ src/routes/mc/favicon.js
@@ -23,15 +23,15 @@ router.get("/:server", (req, res) => {
       let image = canvas.createCanvas(64, 64);
       let ctx = image.getContext("2d");
 
-      await drawImage(
+      drawImage(
         ctx,
         status.favicon || "./src/assets/unknown_pack.png",
         0,
         0,
         64,
         64
-      );
-      sendImage(image, res, "favicon.png");
+      ).then(() => 
+        sendImage(image, res, "favicon.png"));
     })
     .catch((e) => {
       console.log(e);
