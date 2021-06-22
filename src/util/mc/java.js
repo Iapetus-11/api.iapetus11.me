88aa8dc12fb027739b83c0526cec4ec25e5e61ab
Milo Weinberg <iapetus011@gmail.com>
2021-06-22 11:47:34 -0400

    implement java server status using the mcproto library

--- src/util/mc/java.js
+++ src/util/mc/java.js
@@ -1,56 +1,35 @@
-import { MUtf8Decoder, MUtf8Encoder } from "mutf-8";
+import { Client, PacketWriter, State } from "mcproto";
 
-const mutf8Decoder = new MUtf8Decoder();
-const mutf8Encoder = new MUtf8Encoder();
-
-const createServerData = (start, buf) => {
-  const latency = new Date() - start;
-  const data = mutf8Decoder.
-}
-
-const readVarInt = (buf, offset, maxBits) {
-  maxBits = maxBits ? maxBits : 32;
-  const numMin = -1 << (maxBits - 1);
-  const numMax = 1 << (maxBits - 1);
-
-  let num = 0;
-
-  for (let i = 0; i < 10; i++) {
-    let b = this.read(1, offset);
-    offset += 1;
-    num = num | ((b & 0x7f) << (7 * i));
-
-    if (!(b & 0x80)) break;
-  }
-
-  if (num & (1 << 31)) num -= 1 << 32;
-
-  if (!(numMin <= num < numMax)) {
-    throw new RangeError(
-      `${num} doesn't fit in the range ${numMin} <= ${num} < ${numMax}`
-    );
-  }
-
-  return num;
-}
-
-const createStatusHandshakePacket = (host, port) => {
-  let buf = Buffer.from("");
-}
-
-export const javaServerStatus = (host, port) => {
-  return new Promise((resolve, reject) => {
-    const start = new Date();
-
-    const sock = net.createConnection(port, host, () => {
-      sock.write(Buffer.from([0xFE, 0x01]));
-    });
-
-    sock.on("error", (e) => reject(e));
-
-    sock.on("data", (buf) => {
-      sock.destroy();
-      resolve(createServerData(start, buf));
-    });
+export const javaServerStatus = async (host, port) => {
+  const client = await Client.connect(host, port, {
+    connectTimeout: 1000,
+    timeout: 1000
   });
+
+  // send handshake and get basic status
+  client.send(new PacketWriter(0x0).writeVarInt(options.protocol).writeString(host).writeUInt16(port).writeVarInt(State.Status));
+  client.send(new PacketWriter(0x0));
+  status = (await client.nextPacket()).readJSON();
+
+  // get latency / ping as it would appear in the server list
+  client.send(new PacketWriter(0x1).write(Buffer.alloc(8)));
+  const start = Date.now();
+
+  await client.nextPacket(0x1);
+  const latency = Date.now() - start;
+
+  client.end();
+
+  return {
+    online: true,
+    latency: latency,
+    players_online: status.players.online,
+    players_max: status.players.max,
+    players_names: status.players.sample || [],
+    version: { brand: "Java Edition", software: status.version.name, protocol: status.version.protocol },
+    motd: status.description,
+    favicon: status.favicon,
+    map: null,
+    gamemode: null,
+  };
 };
