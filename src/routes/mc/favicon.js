bc05671223a9ce51918d9e96192046778207899f
Milo Weinberg <iapetus011@gmail.com>
2021-06-22 12:33:07 -0400

    add server favicon endpoint

---
+++ src/routes/mc/favicon.js
@@ -0,0 +1,39 @@
+import express from "express";
+
+import { mcStatus, parseAddress } from "../../util/mc/status.js";
+import { drawImage, sendImage } from "../../util/canvas.js";
+
+const router = express.Router();
+
+router.get("/:server", (req, res) => {
+  let address;
+
+  try {
+    address = parseAddress(req.params.server);
+  } catch (e) {
+    res.status(400).json({
+      success: false,
+      message: "Bad Request - URL parameter address is invalid.",
+    });
+    return;
+  }
+
+  mcStatus(...address)
+    .then((status) => {
+      let image = canvas.createCanvas(64, 64);
+      let ctx = image.getContext("2d");
+
+      await drawImage(ctx, status.favicon || "./src/assets/unknown_pack.png", 0, 0, 64, 64);
+      sendImage(image, res, "favicon.png");
+    })
+    .catch((e) => {
+      console.log(e);
+      res.status(500).json({
+        success: false,
+        message:
+          "Error - An error occurred whilst checking the status of the server.",
+      });
+    });
+});
+
+export default router;
